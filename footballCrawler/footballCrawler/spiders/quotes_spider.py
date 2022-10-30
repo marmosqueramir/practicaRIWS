29105ec2d39db8864f7e573bbd0a7b6608a6fadc
Pablo <pabrod17@gmail.com>
2022-10-30 03:37:09 +0100

    BUGFIX: Fix bug when the match has not yet been played

--- footballCrawler/footballCrawler/spiders/quotes_spider.py
+++ footballCrawler/footballCrawler/spiders/quotes_spider.py
@@ -4,17 +4,23 @@ from footballCrawler.items import TutorialItemChampions
 
 class AuthorSpider(scrapy.Spider):
     name = 'matchpoints'
-    start_urls = ['https://www.resultados-futbol.com/primera/grupo1/jornada1', 'https://www.resultados-futbol.com/champions/grupo1/jornada1']
+    start_urls = ['https://www.resultados-futbol.com/primera/grupo1/jornada1', 'https://www.resultados-futbol.com/primera_division_rfef/grupo1/jornada1']
+
+#para diferenciar entre los partidos jugados y los no jugados:
+    #hay que diferenciar entre tr.vevent td.rstd a.url o tr.vevent td.rstd a.hour
+        #si la clase de a es url, entonces se jugo
+        #si la clase de a es hour, entonces no se jugo y habria que cambiar el parse
+
 
     def parse(self, response):
         for i in self.start_urls:
             if(i == 'https://www.resultados-futbol.com/primera/grupo1/jornada1'):
-                matchpoints_page_links = response.css('tr.vevent td.rstd a')
+                matchpoints_page_links = response.css('tr.vevent td.rstd a.url')
                 yield from response.follow_all(matchpoints_page_links, self.parse_author)
 
                 pagination_links = response.css('div.j_sig a')
                 yield from response.follow_all(pagination_links, self.parse)
-            if(i == 'https://www.resultados-futbol.com/champions/grupo1/jornada1'):
+            if(i == 'https://www.resultados-futbol.com/primera_division_rfef/grupo1/jornada1'):
                 matchpoints_page_links = response.css('tr.vevent td.rstd a')
                 yield from response.follow_all(matchpoints_page_links, self.parse_author_champions)
 
@@ -48,7 +54,13 @@ class AuthorSpider(scrapy.Spider):
         def extract_with_css_home_team(query):
             return response.css(query).get()
         def extract_with_css_away_team(query):
-            return response.css(query)[1].get()
+            lista = []
+            lista = response.css(query).getall()
+            if(len(lista) > 0):
+                return response.css(query)[1].get()
+            else:
+                #Hacer otro parse, para los partidos que no se han jugado todavia
+                return 'n/a'
         def extract_with_css_math_result(query):
             return response.css(query).getall()
         
@@ -64,5 +76,5 @@ class AuthorSpider(scrapy.Spider):
         match_item['matchDay']= extract_with_css_home_team('.report-resume-header tbody tr th b::text'),
         match_item['matchSStadium']= extract_with_css_home_team('.stadium b::text'),
         match_item['matchResult']= extract_with_css_math_result('.claseR::text'),
-
         yield match_item
+
