7b420d5b8ba355097dcbfd211b907c139cda3101
marmosqueramir <marmosqueramir@gmail.com>
2022-11-18 02:05:43 +0100

    Fixed fails

--- football-scores-web/src/app/services/elastic-search-players-api.service.ts
+++ football-scores-web/src/app/services/elastic-search-players-api.service.ts
@@ -1,7 +1,6 @@
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
--- football-scores-web/src/environments/environment.ts
+++ football-scores-web/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  host: 'http://localhost:9200',
+  host: 'http://localhost:9200/',
   playerIndexName: 'player',
 };
 
